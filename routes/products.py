2d1f0dc21057c88130324ee877223f04abbee720
MausVII <elanlocke@gmail.com>
2023-05-16 08:45:01 -0400

    freeze

--- routes/products.py
+++ routes/products.py
@@ -1,8 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 from db import get_connection
-import json
 from models import Product
 
 products_bp = Blueprint('products', 'products_bp', url_prefix='products')
@@ -15,3 +14,42 @@ def get_products():
         products = [x for x in session.scalars(query)]
 
         return [p.as_dict() for p in products]
+
+@products_bp.route('/in', methods=['POST'])
+def insert_product():
+    engine = get_connection()
+
+    product_id = request.json.get('product_id')
+    name = request.json.get('name')
+    category = request.json.get('category')
+    price = request.json.get('price')
+    image = request.json.get('image')
+    other_colors = request.json.get('other_colors')
+    short_description = request.json.get('short_description')
+    designer = request.json.get('designer')
+    depth = request.json.get('depth')
+    height = request.json.get('height')
+    width = request.json.get('width')
+
+
+
+    with Session(engine) as session:
+
+        product = Product(
+            id = product_id,
+            name = name,
+            category = category,
+            price = price,
+            image = image,
+            other_colors = other_colors, 
+            short_description = short_description,
+            designer = designer,
+            depth = depth,
+            height = height,
+            width = width
+        )
+
+        session.add(product)
+        session.commit()
+        
+        return "Success"
